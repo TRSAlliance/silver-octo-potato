ddede6ad8980f6145e70573134a67077f205396e
Daniel  Hatton <spanno11@live.com>
2025-08-07 22:46:55 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: './', 
+  base: './',        // relative paths for assets
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist',  // Vercel expects this by your vercel.json
   },
 });
